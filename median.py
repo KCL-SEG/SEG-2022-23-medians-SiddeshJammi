691acb20058e44229c9fefb5d3c32c1da93a9613
Siddesh Jammi <siddesh.jammi@kcl.ac.uk>
2022-10-03 16:12:25 +0100

    Damn bro

--- median.py
+++ median.py
@@ -9,4 +9,19 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+numbers.sort()
 print(numbers)
+n = len(numbers)
+
+if (n % 2) == 0: 
+    n1 = numbers[int(n/2)-1]
+    print(n1)
+    n2 = n1 + 1
+    median = (n1+n2)/2
+    print(n2)
+    print("the median for the list of numbers is: ",median)
+
+elif (n % 2) == 1:
+    median = numbers[int((n-1)/2)]
+    print(median)
