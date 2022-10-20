b2a022fcc7b8121bbd989868bb6a1af426403f73
Siddesh Jammi <siddesh.jammi@gmail.com>
2022-10-20 17:13:48 +0100

    'fixed a minor bug with the code, forgot the placements'

--- median.py
+++ median.py
@@ -17,7 +17,7 @@ n = len(numbers)
 if (n % 2) == 0: 
     n1 = numbers[int(n/2)-1]
     print(n1)
-    n2 = n1 + 1
+    n2 = numbers[int(n/2)]
     median = (n1+n2)/2
     print(n2)
     print("the median for the list of numbers is: ",median)
@@ -25,3 +25,4 @@ if (n % 2) == 0:
 elif (n % 2) == 1:
     median = numbers[int((n-1)/2)]
     print(median)
+
